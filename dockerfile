3fdff011ef72e991efd213acf7a387ef9873e79c
joocer <justin.joyce@joocer.com>
2025-05-06 22:31:15 +0100

    initial

---
+++ dockerfile
@@ -0,0 +1,4 @@
+FROM nginx:latest
+
+COPY src /usr/share/nginx/html
+COPY nginx/default.conf /etc/nginx/conf.d/default.conf
---
+++ run.py
@@ -0,0 +1,21 @@
+import http.server
+import os
+
+DIRECTORY = "src"
+PORT = 8080
+
+class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    directory_index = os.path.join(DIRECTORY, "index.html")
+
+    def translate_path(self, path):
+        return http.server.SimpleHTTPRequestHandler.translate_path(self, f"{DIRECTORY}/{path}")
+    
+with http.server.HTTPServer(("", PORT), CustomHTTPRequestHandler) as httpd:
+    print(f"Serving on port {PORT}...")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("\nServer stopped.")
+        httpd.server_close()
