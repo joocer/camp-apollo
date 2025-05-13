84d2d5d022db0c8d3a275db6a31009e48a3ce955
joocer <justin.joyce@joocer.com>
2025-05-14 00:25:41 +0100

    pub

--- docs/index.md
+++
@@ -1,3 +0,0 @@
-# CAMP APOLLO
-
-Coming Soon.
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import http.server
 import os
 
-DIRECTORY = "src"
+DIRECTORY = "docs"
 PORT = 8080
 
 class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
